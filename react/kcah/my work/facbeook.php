29d24bc33b3003036a083d95036db4074d05d833
Hafiz Bilal <hafiz.bilalahmadgondal@gmail.com>
2021-07-16 20:03:28 +0500

    my first commit after making new repository

---
+++ react/kcah/my work/facbeook.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="stylesheet.css">
+        <title>Facebook - Login</title>
+        <style>
+
+        </style>
+    </head>
+
+    <body style="background-color:#1c1e21;">
+        <!-- <a href="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg">show</a> -->
+        <img src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg" alt="facebook logo">
+    </body>
+    </html>
